10a5cefe1d9cdfb87f09bfc6eb43098f504ae4d1
Mikiyas Tibebu <mikitebe123@gmail.com>
2022-08-12 10:33:41 +0300

    added header menu component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { MantineProvider } from '@mantine/core'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { HeaderResponsive } from "../components/HeaderMenu";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <MantineProvider
-    withGlobalStyles
-    withNormalizeCSS
-    theme={{
-      colorScheme: 'dark',
-    }}
-  >
-    <Component {...pageProps} />
-  </MantineProvider>
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "dark",
+      }}
+    >
+      <HeaderResponsive
+        links={[
+          { link: "/", label: "Amharic OCR" },
+          { link: "/convert", label: "English Amharic to Amharic" },
+        ]}
+      />
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
