8a80fdefcb1f3910ed3d92dbb7169be5a1fcda17
Mikiyas Tibebu <mikitebe123@gmail.com>
2022-08-12 09:39:05 +0300

    fix bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
-import { Group, Stack, Text, Image, Progress, Button } from '@mantine/core';
-import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone';
-import { createWorker } from 'tesseract.js';
+import { useEffect, useRef, useState } from "react";
+import { Group, Stack, Text, Image, Progress, Button } from "@mantine/core";
+import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
+import { createWorker } from "tesseract.js";
 
 const Home = () => {
   const [imageData, setImageData] = useState<null | string>(null);
@@ -15,84 +15,96 @@ const Home = () => {
   };
 
   const [progress, setProgress] = useState(0);
-  const [progressLabel, setProgressLabel] = useState('idle');
-  const [ocrResult, setOcrResult] = useState('');
+  const [progressLabel, setProgressLabel] = useState("idle");
+  const [ocrResult, setOcrResult] = useState("");
 
   const workerRef = useRef<Tesseract.Worker | null>(null);
   useEffect(() => {
     workerRef.current = createWorker({
-      logger: message => {
-        if ('progress' in message) {
+      logger: (message) => {
+        if ("progress" in message) {
           setProgress(message.progress);
-          setProgressLabel(message.progress == 1 ? 'Done' : message.status);
+          setProgressLabel(message.progress == 1 ? "Done" : message.status);
         }
-      }
+      },
     });
     return () => {
       workerRef.current?.terminate();
       workerRef.current = null;
-    }
+    };
   }, []);
 
   const handleExtract = async () => {
     setProgress(0);
-    setProgressLabel('starting');
+    setProgressLabel("starting");
 
     const worker = workerRef.current!;
     await worker.load();
-    await worker.loadLanguage('amh');
-    await worker.initialize('amh');
-    
+    await worker.loadLanguage("amh");
+    await worker.initialize("amh");
+
     const response = await worker.recognize(imageData!);
     setOcrResult(response.data.text);
     console.log(response.data);
   };
-  
-  const handleClear = () => {
-    setImageData(null);
-    setOcrResult('');
-    setProgress(0);
-    setProgressLabel('idle');
-  }
 
   const handleClear = () => {
     setImageData(null);
-    setOcrResult('');
+    setOcrResult("");
     setProgress(0);
-    setProgressLabel('idle');
-  }
+    setProgressLabel("idle");
+  };
 
-  return (<>
-    <Group align='initial' style={{ padding: '10px' }}>
-      <Stack style={{ flex: '1' }}>
-        <Dropzone
-          onDrop={(files) => loadFile(files[0])}
-          accept={IMAGE_MIME_TYPE}
-          multiple={false}
-        >{() => (
-          <Text size="xl" inline>
-            Drag image here or click to select file
-          </Text>
-        )}</Dropzone>
+  return (
+    <>
+      <Group align="initial" style={{ padding: "10px" }}>
+        <Stack style={{ flex: "1" }}>
+          <Dropzone
+            onDrop={(files) => loadFile(files[0])}
+            accept={IMAGE_MIME_TYPE}
+            multiple={false}
+          >
+            {() => (
+              <Text size="xl" inline>
+                Drag image here or click to select file
+              </Text>
+            )}
+          </Dropzone>
 
-        {!!imageData && <Image src={imageData} style={{ width: '100%' }} />}
-      </Stack>
+          {!!imageData && <Image src={imageData} style={{ width: "100%" }} />}
+        </Stack>
 
-      <Stack style={{ flex: '1' }}>
-        <Group grow>
-          <Button disabled={!imageData || !workerRef.current} onClick={handleExtract}>Extract</Button>
-          <Button onClick={handleClear}>Clear</Button>
-        </Group>
-        <Text>{progressLabel.toUpperCase()}</Text>
-        <Progress value={progress * 100} />
+        <Stack style={{ flex: "1" }}>
+          <Group grow>
+            <Button
+              disabled={!imageData || !workerRef.current}
+              onClick={handleExtract}
+            >
+              Extract
+            </Button>
+            <Button onClick={handleClear}>Clear</Button>
+          </Group>
+          <Text>{progressLabel.toUpperCase()}</Text>
+          <Progress value={progress * 100} />
 
-        {!!ocrResult && <Stack>
-          <Text size='xl'>RESULT</Text>
-          <Text style={{ fontFamily: 'monospace', background: 'black', padding: '10px' }}>{ocrResult}</Text>
-        </Stack>}
-      </Stack>
-    </Group>
-  </>);
-}
+          {!!ocrResult && (
+            <Stack>
+              <Text size="xl">RESULT</Text>
+              <Text
+                style={{
+                  fontFamily: "monospace",
+                  background: "black",
+                  padding: "10px",
+                }}
+              >
+                {ocrResult}
+              </Text>
+            </Stack>
+          )}
+        </Stack>
+      </Group>
+    </>
+  );
+};
 
 export default Home;
